1249d0ed8665b206ab26983a5f42c8658452408c
djmoye5 <user.email>
2018-11-07 21:57:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var header = document.querySelector("h2");
+var para = document.querySelector(".p");
+var div = document.querySelector("#div");
+var input = document.querySelector("");
+var button = document.querySelector(".button");
+var sum1 = (10 + 5);
+var x = ("My name is Denerius." + "I am 29 years old." + "I like basketball." + "I am a veteran." + "I love seafood.")
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var string = `"${a}", "${b}", "${c}", "${d}", "${e}"`
+var op1 = 6 % 3
+var op1 = 10 % 2;
+var op1 = 5 % 2;
+array  = [1, 2, 3, 4, "hello", "goodbye", op1 == op2, op2 <= op1, op3 >= op2, [3,"hi", c]]
+var test = {
+    number: 5,
+    string: "hello",
+    array: ["Student", 5, op1]
+  }
+
+<h2></h2>
+<p class="stuff"></p>
+<div id=""></div>
+<input type="text">
+<button>Click Me</button>;
+
+button.addEventListener("click")
